0da52462342471fa1b44c10211302a91d7999c80
Arun Mudgal <58321357+Ar-cell@users.noreply.github.com>
2023-02-07 16:42:56 +0530

    removed unused imports

--- src/main/java/com/inventory/RatingService/serviceImpl/RatingServiceImpl.java
+++ src/main/java/com/inventory/RatingService/serviceImpl/RatingServiceImpl.java
@@ -6,7 +6,6 @@ import com.inventory.RatingService.entity.Rating;
 import com.inventory.RatingService.repository.RatingRepository;
 import com.inventory.RatingService.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
